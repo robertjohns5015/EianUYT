f74feeb4fa2dd8dfe7d101d356afc904b2f49cc9
robertjohns5015 <104138467+robertjohns5015@users.noreply.github.com>
2023-04-19 21:20:55 +0300

    Update convert_laion.py

--- convert_laion.py
+++ convert_laion.py
@@ -3,11 +3,9 @@ import csv
 
 # specify input and output file paths
 input_file = 'laion_synthetic_filtered_large.json'
-output_file = 'laion_synthetic_filtered_large.tsv'
 
 # load JSON data from input file
 with open(input_file, 'r') as f:
-    data = json.load(f)
 
 # extract header and data from JSON
 header = data[0].keys()
